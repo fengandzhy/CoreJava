3a5ae56038b0b27addcb8f8e88c77df5fb572f18
fengandzhy <fengandzhy@gmail.com>
2018-02-01 06:53:49 +0800

    edition1

---
+++ Java/src/main/java/com/citi/hz41382/java/Array/md01/SelectSortTest.java
@@ -0,0 +1,9 @@
+package com.citi.hz41382.java.Array.md01;
+
+public class SelectSortTest {
+
+	public static void main(String[] args) {
+		SelectSort frame = new SelectSort();
+        frame.setVisible(true);
+	}
+}
