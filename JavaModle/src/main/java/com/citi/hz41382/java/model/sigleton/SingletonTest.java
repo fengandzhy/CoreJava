efa196f82baee2ec432767e5d4d050a575d7db7d
fengandzhy <fengandzhy@gmail.com>
2018-03-08 19:29:44 +0800

    other three styles for singleton

---
+++ JavaModle/src/main/java/com/citi/hz41382/java/model/sigleton/SingletonTest.java
@@ -0,0 +1,9 @@
+package com.citi.hz41382.java.model.sigleton;
+
+public class SingletonTest {
+
+	public static void main(String[] args) {
+		Singleton5 singleton = Singleton5.INSTANCE;
+		singleton.doSomething();
+	}
+}
